e2485630665a3f97d5af5f064509c144ebcb2ed6
Iliyan Dimitrov <iliqn1994@gmail.com>
2023-01-09 18:34:43 +0000

    registration form bug fix

--- users/views.py
+++ users/views.py
@@ -26,6 +26,80 @@ def is_client(user):
     else:
         return True
 
+# User Registration
+def register(request):
+    # Making sure the user cannot access registration page if logged in
+    if request.user.is_authenticated is True:
+        return render(request, 'users/profile.html')
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        form_email = form['email'].value()
+        existing_users = User.objects.filter(email=form_email)
+
+        # Existing Email Check
+        if existing_users.count():
+            messages.warning(request, f'Email has been used already!')
+            form = UserRegisterForm()
+            return render(request, 'users/register.html', {'form': form})
+
+        # Validate form input and remove user admin privileges
+        if form.is_valid():
+            user = form.save()
+            # default to non-active
+            user.is_active = False
+            user.is_staff = False
+            user.is_superuser = False
+            user.save()
+
+            # Email details
+            uid = urlsafe_base64_encode(force_bytes(User.objects.last().pk))
+            current_site = get_current_site(request)
+            domain = current_site.domain
+            name = User.objects.last().first_name
+
+            # Site account creation message.
+            messages.success(request, f'Your account has been created. Please confirm your email to finish your registration!')
+
+            # Welcome Email Setup
+            subject = 'Welcome to ETF Exchange'
+            message = "Thanks for having us. Please confirm your email to finish your registration!"
+            sender = settings.EMAIL_HOST_USER
+            receiver = User.objects.last().email
+            password = settings.EMAIL_HOST_PASSWORD
+
+            # Welcome Email Dispatch
+            send_mail(subject, message, sender, [receiver], fail_silently=False, auth_password=password)
+
+            # Confirmation Email Setup
+            email_subject = "Confirm your Email @ ETF Exchange"
+            message2 = render_to_string('users/confirmation_email.html', {
+                'name': user.first_name,
+                'domain': domain,
+                'uid': uid,
+                'token': generate_token.make_token(user)
+            })
+
+            # Confirmation Email Setup
+            email = EmailMessage(
+                email_subject,
+                message2,
+                settings.EMAIL_HOST_USER,
+                [receiver],
+            )
+            # Confirmation Email Dispatch
+            email.fail_silently = True
+            email.send()
+            return redirect('two_factor:login')
+        else:
+            # Error message and registration form reset
+            messages.warning(request, f'Error please try again!')
+            form = UserRegisterForm()
+            return render(request, 'users/register.html', {'form': form})
+    else:
+        form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
+
 # Create a new view for the admin page
 class AdminView(LoginRequiredMixin, TemplateView):
     # Set the template for the view
@@ -91,112 +165,6 @@ def update_user_profile(request, username):
        'p_form': p_form, 
     }
     return render(request, 'users/update_user_profile.html', context)
-
-# def update_user_profile(request, username):
-#     user = User.objects.get(username=username)
-#     # Check if the user is a staff or admin
-#     if not request.user.is_staff and not request.user.is_superuser:
-#         # If not, redirect to the home page
-#         return redirect('home')
-
-#     # Initialize the list of users as an empty list
-#     users = []
-
-#     if request.method == 'POST':
-#         u_form = UserUpdateForm(request.POST, instance=user)
-#         p_form = ProfileUpdateForm(request.POST, 
-#         request.FILES, 
-#         instance=user.profile)
-#         if u_form.is_valid() and p_form.is_valid():
-#             u_form.save()
-#             p_form.save()
-#             messages.success(request, f'User\'s account has been updated.')
-
-#         # Retrieve the list of users based on the search query
-#         if 'username' in request.POST:
-#             users = User.objects.filter(username__icontains=request.POST.get('username'))
-#     else:
-#         u_form = UserUpdateForm(instance=user)
-#         p_form = ProfileUpdateForm(instance=user.profile)
-    
-#     context = {
-#        'u_form': u_form,
-#        'p_form': p_form,
-#        'users': users
-#     }
-#     return render(request, 'users/update_user_profile.html', context)
-
-def register(request):
-    #Making sure the user cannot access registeration page if logged in 
-    if request.user.is_authenticated is True:
-        return render(request, 'users/profile.html')
-    
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        form_email = form['email'].value()
-        existing_users = User.objects.filter(email=form_email)
-
-        # Existing Email Check
-        if existing_users.count():
-            messages.warning(request, f'Email has been used already!')
-            form = UserRegisterForm()
-            return render(request, 'users/register.html', {'form': form})
-
-        # Validate form input and remove user admin priviliges
-        if form.is_valid():
-            user = form.save()
-            # default to non-active
-            user.is_active = False
-            user.is_staff = False
-            user.is_superuser = False
-            user.save()
-
-            #Email details
-            uid = urlsafe_base64_encode(force_bytes(User.objects.last().pk))
-            current_site = get_current_site(request)
-            domain = current_site.domain
-            name = User.objects.last().first_name
-
-            # Site account creation message.
-            messages.success(
-                request, f'Your account has been created. Please confirm your email to finish your registration!')
-
-            # Welcome Email Setup
-            subject = 'Welcome to ETF Exchange'
-            message = "Thanks for having us. Please confirm your email to finish your registration!"
-            sender = settings.EMAIL_HOST_USER
-            receiver = User.objects.last().email
-            password = settings.EMAIL_HOST_PASSWORD
-
-            # Welcome Email Dispatch
-            send_mail(subject, message, sender, [receiver], fail_silently=False, auth_password=password)
-
-            # Confiramation Email Setup
-            email_subject = "Confirm your Email @ ETF Exchange"
-            message2 = render_to_string('users/confirmation_email.html', {
-                'name': user.first_name,
-                'domain': domain,
-                'uid': uid,
-                'token': generate_token.make_token(user)
-            })
-
-            # Confiramation Email Setup
-            email = EmailMessage(
-                email_subject,
-                message2,
-                settings.EMAIL_HOST_USER,
-                [receiver],
-            )
-            # Confiramation Email Dispatch
-            email.fail_silently = True
-            email.send()
-            return redirect('two_factor:login')
-    else:
-        #Error message and registration form reset
-        messages.warning(
-                     request, f'Error please try again!')
-        form = UserRegisterForm()
-        return render(request, 'users/register.html', {'form': form})
 
 # Profile protected route
 @login_required
